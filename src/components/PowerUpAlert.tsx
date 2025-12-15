9938c1ef9d13c77d230fa7608d22f351d451ca41
snackman <samgold24@gmail.com>
2025-12-15 08:10:41 -0500

    Updated PowerUpAlert.tsx

--- src/components/PowerUpAlert.tsx
+++ src/components/PowerUpAlert.tsx
@@ -6,13 +6,13 @@ interface PowerUpAlertProps {
   chefLane: number;
 }
 
-const PowerUpAlert: React.FC<PowerUpAlertProps> = ({ powerUpType, chefLane }) => {
+const PowerUpAlert: React.FC<PowerUpAlertProps> = ({ powerUpType }) => {
   const getAlertContent = () => {
     switch (powerUpType) {
       case 'doge':
         return {
           image: 'https://i.imgur.com/n0FtlUg.png',
-          scale: 3,
+          scale: 6,
         };
       default:
         return null;
@@ -22,34 +22,23 @@ const PowerUpAlert: React.FC<PowerUpAlertProps> = ({ powerUpType, chefLane }) =>
   const content = getAlertContent();
   if (!content) return null;
 
-  const isLandscape = typeof window !== 'undefined' ? window.innerWidth > window.innerHeight : false;
-  
-  let chefTopPosition, chefLeftPosition;
-  if (isLandscape) {
-    chefTopPosition = 30 + chefLane * 20;
-    chefLeftPosition = 20;
-  } else {
-    chefTopPosition = chefLane * 25 + 13;
-    chefLeftPosition = 10;
-  }
-
   const scale = content.scale || 1;
 
   return (
-    <div 
+    <div
       className="absolute pointer-events-none z-10"
       style={{
-        top: `${chefTopPosition}%`,
-        left: `${chefLeftPosition}%`,
+        top: '50%',
+        left: '50%',
         transform: `translate(-50%, -50%) scale(${scale})`,
         transformOrigin: 'center center',
       }}
     >
       <div className={`p-4 text-center animate-pulse`}>
         <div className="mb-2">
-          <img 
-            src={content.image} 
-            alt={powerUpType} 
+          <img
+            src={content.image}
+            alt={powerUpType}
             className="w-16 h-16 mx-auto object-contain"
           />
         </div>
