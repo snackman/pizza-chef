e779b5116f9e3f3c89916a744b87a12b3c38ac7c
snackman <samgold24@gmail.com>
2025-12-15 15:54:02 -0500

    Updated PowerUp.tsx

--- src/components/PowerUp.tsx
+++ src/components/PowerUp.tsx
@@ -8,36 +8,8 @@ interface PowerUpProps {
   powerUp: PowerUpType;
 }
 
-const LANDSCAPE_LANE_POSITIONS = [20, 40, 60, 80]; // same as LandscapeCustomer
-
 const PowerUp: React.FC<PowerUpProps> = ({ powerUp }) => {
   const leftPosition = powerUp.position;
-
-  // Helpers that are safe with SSR
-  const getIsLandscape = () =>
-    typeof window !== 'undefined' ? window.innerWidth > window.innerHeight : true;
-
-  const getIsMobile = () =>
-    typeof navigator !== 'undefined'
-      ? /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent) ||
-        (navigator as any).maxTouchPoints > 1
-      : false;
-
-  const [isLandscape, setIsLandscape] = React.useState(getIsLandscape);
-  const [isMobile, setIsMobile] = React.useState(getIsMobile);
-
-  React.useEffect(() => {
-    const onChange = () => {
-      setIsLandscape(getIsLandscape());
-      setIsMobile(getIsMobile());
-    };
-    window.addEventListener('resize', onChange);
-    window.addEventListener('orientationchange', onChange);
-    return () => {
-      window.removeEventListener('resize', onChange);
-      window.removeEventListener('orientationchange', onChange);
-    };
-  }, []);
 
   const getImage = () => {
     switch (powerUp.type) {
@@ -59,11 +31,7 @@ const PowerUp: React.FC<PowerUpProps> = ({ powerUp }) => {
 
   const image = getImage();
 
-  // Only snap to LandscapeCustomer positions when mobile + landscape
-  const topPercent =
-    isMobile && isLandscape
-      ? LANDSCAPE_LANE_POSITIONS[powerUp.lane]
-      : powerUp.lane * 25 + 6;
+  const topPercent = powerUp.lane * 25 + 6;
 
   return (
     <div
