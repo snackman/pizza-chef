6300fc5c96625ffb8f75204caad7d58a63b56d3b
snackman <samgold24@gmail.com>
2025-12-21 00:22:41 -0500

    Updated DebugPanel.tsx

--- src/components/DebugPanel.tsx
+++ src/components/DebugPanel.tsx
@@ -68,7 +68,7 @@ const DebugPanel: React.FC<DebugPanelProps> = ({
               <img
                 src={icon}
                 alt={label}
-                className="relative z-10 w-7 h-7 object-contain"
+                className="relative z-10 w-8 h-8 object-contain"
                 draggable={false}
               />
 
