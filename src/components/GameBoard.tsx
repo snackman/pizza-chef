89ae0133027ba9f7032cd2ab161a5d22dc8ba4ea
snackman <samgold24@gmail.com>
2025-12-28 11:37:07 -0500

    Updated GameBoard.tsx

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -176,7 +176,7 @@ const GameBoard: React.FC<GameBoardProps> = ({ gameState }) => {
           <div
             className={`absolute ${gameState.starPowerActive ? 'animate-pulse' : ''}`}
             style={{
-              left: '68%',
+              left: '60%',
               top: '60%',
               width: '91%',
               height: '91%',
