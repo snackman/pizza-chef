f035c76d2b0ae5c7e97768a706020e448668cf1a
snackman <samgold24@gmail.com>
2025-12-29 22:11:54 -0500

    Updated useGameLogic.ts

--- src/hooks/useGameLogic.ts
+++ src/hooks/useGameLogic.ts
@@ -881,7 +881,7 @@ export const useGameLogic = (gameStarted: boolean = true) => {
         // Check pizza-customer collisions first
         newState.customers = newState.customers.map(customer => {
           // Skip customers that are leaving (disappointed or vomit or brian)
-          if (customer.disappointed || customer.vomit) {
+          if (customer.disappointed || customer.vomit || customer.leaving) {
             return customer;
           }
 
