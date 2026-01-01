06f7e6c7e1bea4a1263e759994e438fae79aeb81
snackman <samgold24@gmail.com>
2026-01-01 12:49:49 -0500

    Updated GameBoard.tsx

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -9,7 +9,7 @@ import FloatingScore from './FloatingScore';
 import Boss from './Boss';
 import { GameState } from '../types/game';
 import pizzaShopBg from '/pizza shop background v2.png';
-import chefImg from '/Sprites/chefemoji.png';
+import chefImg from '/sprites/chefemoji.png';
 
 interface GameBoardProps {
   gameState: GameState;
