154973dd6c336013c24c54ba474aa591f6a1b8be
snackman <samgold24@gmail.com>
2026-01-02 11:24:42 -0500

    Updated SplashScreen.tsx

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -10,12 +10,13 @@ interface SplashScreenProps {
 const SplashScreen: React.FC<SplashScreenProps> = ({ onStart }) => {
   return (
     <div className="fixed inset-0 bg-red-600 flex items-center justify-center z-50">
-      <div className="text-center space-y-3 p-8">
-          <img
+      <div className="text-center space-y-3 p-8 relative">
+        <img
           src={"https://i.imgur.com/EPCSa79.png"}
           alt="PizzaDAO Logo"
           className="w-48 h-auto mx-auto mb-5"
         />
+
         <h1 className="text-6xl font-bold text-white mb-4">
           Pizza Chef
         </h1>
@@ -32,6 +33,20 @@ const SplashScreen: React.FC<SplashScreenProps> = ({ onStart }) => {
         >
           Start Game
         </button>
+
+        {/* GitHub link */}
+        <a
+          href="https://github.com/snackman/pizza-chef"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="block mt-6 opacity-80 hover:opacity-100 transition"
+        >
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+            alt="GitHub"
+            className="w-8 h-8 mx-auto"
+          />
+        </a>
       </div>
     </div>
   );
